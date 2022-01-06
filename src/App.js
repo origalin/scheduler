80ccd8780d79b2ff15c179f50de3e4934d97fdd5
origalin <2360436350@qq.com>
2022-01-05 22:17:01 -0600

    read from a database

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import CourseList from './components/CourseList';
 import './App.css';
+import { useData } from './utilities/firebase.js';
 
-const schedule = {
-    "title": "CS Courses for 2018-2019",
-    "courses": {
-        "F101" : {
-            "id" : "F101",
-            "meets" : "MWF 11:00-11:50",
-            "title" : "Computer Science: Concepts, Philosophy, and Connections"
-        },
-        "F110" : {
-            "id" : "F110",
-            "meets" : "MWF 10:00-10:50",
-            "title" : "Intro Programming for non-majors"
-        },
-        "S313" : {
-            "id" : "S313",
-            "meets" : "TuTh 15:30-16:50",
-            "title" : "Tangible Interaction Design and Learning"
-        },
-        "S314" : {
-            "id" : "S314",
-            "meets" : "TuTh 9:30-10:50",
-            "title" : "Tech & Human Interaction"
-        }
-    }
-};
 
 const App = () => {
-    const [schedule, setSchedule] = useState();
-    const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+    const [schedule, loading, error] = useData('/', addScheduleTimes);
 
-    useEffect(() => {
-        const fetchSchedule = async () => {
-            const response = await fetch(url);
-            if (!response.ok) throw response;
-            const json = await response.json();
-            setSchedule(addScheduleTimes(json));
-        }
-        fetchSchedule();
-    }, []);
-
-    if (!schedule) return <h1>Loading schedule...</h1>;
+    if (error) return <h1>{error}</h1>;
+    if (loading) return <h1>Loading the schedule...</h1>
 
     return (
         <div className="container">
@@ -55,8 +21,6 @@ const App = () => {
 const Banner = props => (
     <h1>{props.title}</h1>
 )
-
-
 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
---
+++ src/utilities/firebase.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import {useEffect, useState} from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBo4XVJVe3uetguBYuA2XAl1su6DViSEAk",
+    authDomain: "quickreact-90efb.firebaseapp.com",
+    databaseURL: "https://quickreact-90efb-default-rtdb.firebaseio.com",
+    projectId: "quickreact-90efb",
+    storageBucket: "quickreact-90efb.appspot.com",
+    messagingSenderId: "600089176792",
+    appId: "1:600089176792:web:31aaa035982f4738f8c5ad",
+    measurementId: "G-XMJ1B7BRJV"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+        if (devMode) { console.log(`loading ${path}`); }
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            if (devMode) { console.log(val); }
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+
+    return [data, loading, error];
+};
